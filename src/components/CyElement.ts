24944c470f2a7582433368af5a554b068d9d1a07
William <william.fischer42@gmail.com>
2019-11-13 10:26:49 -0700

    Added data & position reactivity to elements.

--- src/components/CyElement.ts
+++ src/components/CyElement.ts
@@ -1,4 +1,4 @@
-import { Component, Vue, Prop, Inject } from 'vue-property-decorator'
+import { Component, Vue, Prop, Inject, Watch } from 'vue-property-decorator'
 import {
   Core,
   ElementDefinition,
@@ -13,17 +13,23 @@ export default class CyElement extends Vue {
   @Inject() readonly cy!: Promise<Core>
   instance: Core | undefined = undefined
   selector: Selector = ''
-  @Prop({ default: false }) readonly sync!:boolean
+  id: string | undefined = this.definition.data.id
+  @Prop({ default: false }) readonly sync!: boolean
 
   constructor() {
     super()
-    this.selector = `#${this.definition.data.id}`
+    if (this.id) this.selector = `#${this.id}`
     this.cy.then(this.configure)
   }
 
   configure(cy: Core) {
     this.instance = cy
-    this.add()
+    const ele = this.add()
+    console.log(ele)
+    if (!this.id) {
+      this.id = ele.data().id
+      this.selector = `#${this.id}`
+    }
   }
 
   add() {
@@ -38,19 +44,49 @@ export default class CyElement extends Vue {
       else register(eventType, callback as EventHandler)
     }
     // if sync is on, track position
-    if(this.sync) {
+    if (this.sync) {
       instance.on('drag', this.selector, event => {
+        // strip observers from the event position
+        const pos = JSON.parse(JSON.stringify(event.target.position()))
         // update definition object
-        this.definition.position = event.target.position()
+        this.definition.position = pos
       })
     }
+
+    // strip observers from the original definition
+    let def = JSON.parse(JSON.stringify(this.definition))
     // add the element to cytoscape
-    instance.add(this.definition)
+    const ele = instance.add(def)[0]
+    return ele
   }
 
   unmounted() {
     const instance = this.instance as Core
     instance.remove(this.selector)
+  }
+
+  get eleData() {
+    return this.definition.data
+  }
+
+  get position() {
+    return this.definition.position
+  }
+
+  @Watch('eleData', { deep: true })
+  onDataChange(data: any) {
+    const instance = this.instance as Core
+    const ele = instance.getElementById(this.id as string)
+    ele.data(data)
+    // console.log(ele.data().label)
+  }
+
+  @Watch('position', { deep: true })
+  onPositionChange(position: any) {
+    const instance = this.instance as Core
+    const ele = instance.getElementById(this.id as string)
+    ele.position(JSON.parse(JSON.stringify(position)))
+    console.log('watcher')
   }
 
   render(h: (arg0: string) => void) {
